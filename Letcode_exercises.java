da91806803647e2c2f6ca51b943318354dad7ebd
Muthu Prabha <100946742+sprabhas@users.noreply.github.com>
2025-05-25 22:08:01 +0530

    Add files via upload

--- Letcode_exercises.java
+++ Letcode_exercises.java
@@ -2,11 +2,13 @@ package NaveenAutomationLabs.NaveenAutomationSite;
 
 import java.time.Duration;
 import java.util.Set;
+import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Letcode_exercises {
 
@@ -17,11 +19,14 @@ public class Letcode_exercises {
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		
-		/*driver.get("https://letcode.in/table");
+		
+		driver.get("https://letcode.in/table");
 		int itemsSum = 0;
-		List<WebElement> prices = driver.findElements(By.xpath("//table[@id='shopping']//tbody//td[2]"));
+		
+		List<WebElement> prices = driver.findElements(By.cssSelector("table[id='shopping'] tbody td:nth-child(2)"));
 		for (WebElement itemPrice  : prices) {
 			
 			itemsSum += Integer.parseInt(itemPrice.getText());
@@ -32,7 +37,7 @@ public class Letcode_exercises {
 		// System.out.println(itemsSubtotal);
 		 if(itemsSum == itemsSubtotal) {
 			 System.out.println("true");
-		 }*/
+		 }
 		 
 		 //Ex - 2
 		 // Click on an element having property like name and title	 
@@ -41,7 +46,7 @@ public class Letcode_exercises {
 		//Ex - 3
 		// switch between windows
 		
-		driver.get("https://letcode.in/test");
+		 /*driver.get("https://letcode.in/test");
 		
 		js.executeScript("window.scrollBy(0, 500)");
 		
@@ -54,7 +59,7 @@ public class Letcode_exercises {
 				
 		String parentWindow = driver.getWindowHandle();
 		
-		Set<String> windowsList = driver.getWindowHandles();
+		 Set<String> windowsList = driver.getWindowHandles();
 		
 		for (String individualWindows : windowsList) {
 			if(!parentWindow.equals(individualWindows)) {
@@ -66,7 +71,10 @@ public class Letcode_exercises {
 				
 			}
 			
-		}		
+		}	*/
+		
+		
+		//@findby , testng 
 	}
 
 }
