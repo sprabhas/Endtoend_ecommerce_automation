52bc0f6230978529da4aeff8819fe8d72b374493
Muthu Prabha <100946742+sprabhas@users.noreply.github.com>
2025-06-13 22:15:51 +0530

    Update Sausedemo_practice.java

--- Sausedemo_practice.java
+++ Sausedemo_practice.java
@@ -20,13 +20,17 @@ public class Sausedemo_practice {
 		
 		 WebDriver driver = new ChromeDriver();
 		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));		 
-		 JavascriptExecutor js = (JavascriptExecutor)driver;		 
-		 driver.get("https://www.saucedemo.com/");
+		 		 
+		 
+		 LandingPage LandingPage = new LandingPage(driver);
+		 LandingPage.goTo();
+		 LandingPage.loginApplication("standard_user", "secret_sauce");
+		 
 		 String pageTitle = driver.getTitle();
 		 
 		 Assert.assertEquals(pageTitle, "Swag Labs" , "Page title is incorrect");
 						
-		 String btnText = driver.findElement(By.id("login-button")).getDomAttribute("value");
+		/* String btnText = driver.findElement(By.id("login-button")).getDomAttribute("value");
 		 
 		 //if((btnText.substring(0,1).toUpperCase()+btnText.substring(1)).equals(btnText)) {
 		 		 
@@ -34,74 +38,58 @@ public class Sausedemo_practice {
 			 System.out.println("Button is capitalized!");
 		 } else {		 
 			 System.out.println("Button is not capitalized!");
-		 }
-				 
-		 driver.findElement(By.id("user-name")).sendKeys("standard_user");
-		 driver.findElement(By.id("password")).sendKeys("secret_sauce");
-		 driver.findElement(By.id("login-button")).click();
+		 }*/
+				  
+		// driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		// driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		 //driver.findElement(By.id("login-button")).click();
 		
 		 WebElement selFil = driver.findElement(By.className("product_sort_container"));
 		 
 		 Select selFilter = new Select(selFil);
-		 
-		 
+		  
 		 WebElement firstOption = selFilter.getFirstSelectedOption();	 
 		 Assert.assertEquals(firstOption.getText(), "Name (A to Z)" , "The option A to Z is not selected");
+		 		
+		 ProductCatalogue ProductCatalogue = new ProductCatalogue(driver);
+		 ProductCatalogue.addProductToCart(ProductCatalogue.backpack);	 
+		 //System.out.println(ProductCatalogue.backpack);
+	     String cartCount1 = ProductCatalogue.getCartItemCount();
+	     Assert.assertEquals(cartCount1, "1" , "items in cart doesn't equals 1");
 		 
-		/* if(firstOption.getText().equals("Name (A to Z)")) {
-			 System.out.println("az is selected");
-		 }
-		 else {
-			 System.out.println("az is not selected");
-			 
-		 }*/
 		 
-		 driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
-		 String cartNumber1 = driver.findElement(By.className("shopping_cart_badge")).getText();		 
-		 Assert.assertEquals(cartNumber1, "1" , "items in cart doesn't equals 1");
+		 ProductCatalogue.addProductToCart(ProductCatalogue.redTshirt);
+		 String cartCount2 = ProductCatalogue.getCartItemCount();
+		 Assert.assertEquals(cartCount2, "2" , "items in cart is not increased");
 		 
-		 js.executeScript("window.scrollBy(0,400)");
-		 driver.findElement(By.id("add-to-cart-test.allthethings()-t-shirt-(red)")).click();	 
-		 js.executeScript("window.scrollBy(0,-600)");
-		 		 
-		 String increasedcartNumber = driver.findElement(By.className("shopping_cart_badge")).getText();			 
-		 Assert.assertEquals(increasedcartNumber, "2" , "items in cart is not increased");
+		 ProductCatalogue.addProductToCart(ProductCatalogue.removeItem);
+		 String updCart = ProductCatalogue.getCartItemCount();		
+	     //System.out.println(updCart);
+		 Assert.assertEquals(updCart, "1" , "items in cart is not updated");
+		 		
+		 String selecteditemName = ProductCatalogue.getItemName();
 		 
-		 driver.findElement(By.id("remove-sauce-labs-backpack")).click();
-		 
-		 String updatedcartNumber = driver.findElement(By.className("shopping_cart_badge")).getText();
-		 Assert.assertEquals(updatedcartNumber, "1" , "items in cart is not updated");
-		
-		 driver.findElement(By.className("shopping_cart_badge")).click();
-		 
-	     String selectedItemText = driver.findElement(By.cssSelector("a[id='item_3_title_link']>div")).getText();
-	     	     
-	     String cartItemslist = driver.findElement(By.cssSelector("a[id='item_3_title_link']>div")).getText();
-	     
-	     Assert.assertEquals(selectedItemText, cartItemslist , "items are in not in cart");
+		 ProductCatalogue.clickCart(ProductCatalogue.shopping_cart_badge); 
+		 String cartitemName = ProductCatalogue.getItemName();
+	     Assert.assertEquals(selecteditemName, cartitemName , "items are in not in cart");
+	       
 	     driver.findElement(By.id("continue-shopping")).click();
 	     	     
-	     WebElement sortFilter = driver.findElement(By.className("product_sort_container"));		 
-		 Select sortFiltersel = new Select(sortFilter);
-		 sortFiltersel.selectByValue("lohi");
+	     //driver.findElement(By.xpath("//select[@class='product_sort_container']//option[@value='lohi']"));
 		 
-		////select[@class='product_sort_container']//option[@value='lohi']
-	    
-	     List<WebElement> itemsPrice = driver.findElements(By.className("inventory_item_price"));
-	     
+	     ProductCatalogue.selectloHiFilter(ProductCatalogue.loHiFilter);
 	     ArrayList<Double> priceList = new ArrayList();
+	     
+	     
+	     List<WebElement> itemsPrice = driver.findElements(By.className("inventory_item_price"));
 	     	    
-	     for (int i = 0 ; i < itemsPrice.size() ; i ++ ) {
+	     for (int i = 0 ; i < ProductCatalogue.get_inventory_item_price_size() ; i ++ ) {
 	    	 
-	    	 double individualPrice = Double.parseDouble( itemsPrice.get(i).getText().substring(1));	    			
+	    	 double individualPrice = Double.parseDouble( ProductCatalogue.inventory_item_price.get(i).getText().substring(1));	    			
 	    	 priceList.add(individualPrice);
 	    	     	
 	     }  
-	     
-	     
-	     //[4,5,6,45]
-	    		 
-	    		 
+	     	     		 	    		 
 	     boolean isSorted = true;
 	     for(int j = 0 ; j < priceList.size()-1 ; j ++) {
 	    	 if(priceList.get(j) > priceList.get(j + 1)) {
